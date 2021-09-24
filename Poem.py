1ce9dce08519e294a4d0f9fa51574818ede53144
AbdelMa2004 <34225@ma-web.nl>
2021-09-24 09:59:48 +0200

    Poem.py

---
+++ Poem.py
@@ -0,0 +1,11 @@
+vers1 = "ik ben Abdelhay "
+
+vers2 = "in de zee ben ik een haai. "
+
+vers3 = "ik woon in Amsterdam. "
+
+vers4 = "ik sliep toen me moeder iets wegnam."
+
+vers5 = "heb een hekel aan muggen en ook aan bruggen. "
+
+print(vers1, vers2, vers3, vers4, vers5)
