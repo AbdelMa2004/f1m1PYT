269f325c340cfe687e55926c1d19f5cb0c6156be
AbdelMa2004 <34225@ma-web.nl>
2021-10-01 10:17:17 +0200

    Player

---
+++ playerstats.py
@@ -0,0 +1,31 @@
+print("Name: Monkey D.Luffy ")
+
+print("Level: 6969 ")
+
+print("Speed: Gear 2")
+
+print("Devil Fruit: GUM-GUM no mi")
+
+print("Transformation: Gear 4: Bounce Man, Tank Man, Snake Man")
+
+print("Haki: Haoshoku ")
+
+print("Race: Human")
+
+print("Total Money: $100.000.000")
+
+print("Bounty: $1.500.000.000")
+
+print("Crew Name: Strawhat Pirates")
+
+print("Crew members: 9 ")
+
+print("Boat: Thousand Sunny")
+
+
+
+
+
+
+
+print("")
