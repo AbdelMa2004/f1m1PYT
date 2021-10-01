b9e9e127c6208fdeef30b29519c133c029aa20bd
AbdelMa2004 <34225@ma-web.nl>
2021-10-01 10:35:49 +0200

    Berekening

---
+++ opperation.py
@@ -0,0 +1,18 @@
+print( 5 % 4)
+print(12 + 456)
+print(45 - 23)
+print(890 * 69)
+print(60/32)
+print(6 ** 7)
+print(15 // 4)
+i = 2
+i += 8
+print(i)
+p = 4
+p /= 23
+print(p)
+q = 8
+q != 90
+print(q)
+
+
