07987f1712c2da5e010957eb5be1da61ed6161ea
AbdelMa2004 <34225@ma-web.nl>
2021-10-29 09:37:53 +0200

    vragenstellen

---
+++ vragenstellen.py
@@ -0,0 +1,10 @@
+antwoord = input("Hoe heet jij?")
+print("Hallo " + antwoord)
+
+antwoord1 = input("Hoe oud ben je?")
+print("oh oke dus je bent " + antwoord1 + "jaar oud, dat is leuk.")
+
+antwoord2 = input("Waar woon je?")
+print("dus je woont in " + antwoord2 + " dat is leuk zeg.")
+
+print("Hey " + antwoord + " je bent alweer " + antwoord1 + " jaar oud en je woont volgens mij in " + antwoord2 + " of niet?")
