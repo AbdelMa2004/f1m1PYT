d2a5a1e10606546c0b653bbab1fa110607d331a0
AbdelMa2004 <34225@ma-web.nl>
2021-11-02 11:54:37 +0100

    transport items commit 1

---
+++ transport_items.py
@@ -0,0 +1,38 @@
+import os
+import time
+
+Factory = []
+Distribution = []
+shop = []
+item = "apple"
+#Factory.insert(1, item)
+
+print("Factory:",Factory )
+print("Distribution:",Distribution)
+print("shop:",shop)
+
+time.sleep(1)
+os.system('cls')
+
+Factory.insert(0, item)
+print("Factory:",Factory )
+print("Distribution:",Distribution)
+print("shop:",shop)
+
+time.sleep(1)
+os.system('cls')
+
+Distribution.insert(0, item)
+Factory.pop(0)
+print("Factory:",Factory,)
+print("Distribution:",Distribution)
+print("shop:",shop)
+
+time.sleep(1)
+os.system('cls')
+
+Distribution.pop(0)
+shop.insert(1, item)
+print("Factory:", Factory )
+print("Distribution:",Distribution)
+print("shop:",shop)
