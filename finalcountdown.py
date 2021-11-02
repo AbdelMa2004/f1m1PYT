a9564949f859447df2ef981287c062cdd31d9a6a
AbdelMa2004 <34225@ma-web.nl>
2021-11-02 11:52:21 +0100

    finalcountdown commit 1

---
+++ finalcountdown.py
@@ -0,0 +1,11 @@
+import time
+countdown = 20
+print (countdown)
+while 5 > 1:
+    countdown -= 1
+    time.sleep(1)
+    print (countdown)
+    if countdown == 0:
+        if countdown == 0:
+            import sys
+            sys.exit()
